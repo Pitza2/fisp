c90f92da1b7fe357a7c41f5cacc45546e474e321
Lica Andrei <ovidiu.lica@student.upt.ro>
2023-10-17 17:39:56 +0300

    Untrack files in .gitignore

--- AppFrontend/src/app/app-routing.module.ts
+++ AppFrontend/src/app/app-routing.module.ts
@@ -1,34 +1,45 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {Router, Routes} from "@angular/router";
-import {RouterModule} from "@angular/router";
-import {StudentListComponent} from "./components/Students/student-list/student-list.component";
-import{LoginPageComponent} from "./components/login-page/login-page.component";
-import {RegisterPageComponent} from "./components/register-page/register-page.component";
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { Routes } from '@angular/router'
+import { RouterModule } from '@angular/router'
+import { LoginPageComponent } from './components/login-page/login-page.component'
+import { RegisterPageComponent } from './components/register-page/register-page.component'
 import { ApplicantPageComponent } from './components/applicant-page/applicant-page.component'
+import { CompanyPageComponent } from './components/company-page/company-page.component'
 
-const routes : Routes =[
+const routes: Routes = [
   {
-    path:'',component:LoginPageComponent
+    path: '',
+    component: LoginPageComponent
   },
   {
-    path:'login',component:LoginPageComponent
+    path: 'login',
+    component: LoginPageComponent
   },
   {
-    path:'register',component:RegisterPageComponent
+    path: 'register',
+    component: RegisterPageComponent
   },
   {
-    path:'home',component:ApplicantPageComponent
+    path: 'home/applicant',
+    component: ApplicantPageComponent
+  },
+
+  {
+    path: 'home/company',
+    component: CompanyPageComponent
   }
 ]
+
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     RouterModule.forRoot(routes)
   ],
-  exports:[
+  exports: [
     RouterModule
   ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
